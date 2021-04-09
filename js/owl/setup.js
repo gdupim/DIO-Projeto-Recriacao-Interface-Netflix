36f5ff9967b39e796adffc5e03737034a93a2173
Gabriel Dupim <gabrdupim@gmail.com>
2021-04-08 22:32:10 -0300

    adiciona jQuery Owl-Carousel a pasta js e mais código ao index.html, main.css e setup.js

---
+++ js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false, //? Setas de navegação
+    responsive: {
+        0: { //? Se for menor que 600px só uma imagem aparecerá
+            items: 1
+        },
+        600: { //? Se for menor ou igual que 600px, só três imagens aparecerarão
+            items: 3
+        },
+        1000: { //? Se for maior ou igual que 1000px, só cinco imagens aparecer
+            items: 5
+        }
+    }
+})
