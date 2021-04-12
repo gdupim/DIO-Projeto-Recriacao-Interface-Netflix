e3b7c6e8fbb911740ecd71a85ecd4898074c1608
Gabriel Dupim <gabrdupim@gmail.com>
2021-04-11 23:02:02 -0300

    adiciona imagens, pastas series, documentarios e destasques e código ao index.html, setup.js e main.css

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -14,3 +14,22 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+$('.owl-carousel-principal').owlCarousel({
+    loop: false,
+    margin: 10,
+    autoplay: true,
+    dots: false,
+    nav: false, //? Setas de navegação
+    responsive: {
+        0: { //? Se for menor que 600px só uma imagem aparecerá
+            items: 1
+        },
+        600: { //? Se for menor ou igual que 600px, só três imagens aparecerarão
+            items: 3
+        },
+        1000: { //? Se for maior ou igual que 1000px, só cinco imagens aparecer
+            items: 5
+        }
+    }
+})
