59443d33aae9a493b3927b5ae50c07d08a5deccb
Gabriel Dupim <gabrdupim@gmail.com>
2021-04-19 19:31:56 -0300

    adiciona código para um carrosel de destaques, responsividade e retoques finais

--- js/main.js
+++ js/main.js
@@ -41,21 +41,34 @@ let altsDocs = ["Último Cruzeiro", "Educação Americana: Fraude e Privilégio"
                 "Crack: Cocaína, Corrupção e Conspiração", "Visita ao Inferno", "Seaspiracy: Mar Vermelho", "O Dilema das Redes", 
                 "If It Bleeds We Can Kill It: The Making of \'Predator\'", "David Attenborough e Nosso Planeta", "The Story of Star Wars"];
 
-let destaquesLinks = ["img/destaques/filme-destaque1.jpg", "img/destaques/filme-destaque2.jpg", "img/destaques/filme-destaque3.jpg", 
+let linksDestaques = ["img/destaques/filme-destaque1.jpg", "img/destaques/filme-destaque2.jpg", "img/destaques/filme-destaque3.jpg", 
                       "img/destaques/serie-destaque1.jpg", "img/destaques/serie-destaque2.jpg", "img/destaques/serie-destaque3.jpg", 
                       "img/destaques/documentario-destaque1.jpg", "img/destaques/documentario-destaque2.jpg", "img/destaques/documentario-destaque3.jpg"];
 
-let destaquesHrefPlays = ["https://www.youtube.com/watch?v=kIET9yMABD8", "https://www.youtube.com/watch?v=uXm6T1rAe28", 
-                          "https://www.youtube.com/watch?v=jt3-3LtSj6c", "https://www.youtube.com/watch?v=ZHaokMTWgdY", 
-                          "https://www.youtube.com/watch?v=e43b-f_Vg9I&pp=qAMBugMGCgJwdBAB", "https://www.youtube.com/watch?v=p__jH6NkowU",
-                          "https://www.youtube.com/watch?v=zFfurx3RZqo", "https://www.youtube.com/watch?v=iG5ni4b9QI0",
-                          "https://www.youtube.com/watch?v=LFHj8e7mU_I"];
+let tituloDestaques = ["Godzilla Vs Kong", "Tenet", "Liga da Justiça: Snyder's Cut", "Falcão e o Soldado Invernal", "O Flash", "Wandavision", 
+                       "Seremos História?", "O Último Cruzeiro", "Educação Americana: Fraude e Privilégio"];
 
-let destaquesHrefInfos = ["https://www.themoviedb.org/movie/399566-godzilla-vs-kong", "https://www.themoviedb.org/movie/577922-tenet",
-                          "https://www.themoviedb.org/movie/791373-zack-snyder-s-justice-league", 
-                          "https://www.themoviedb.org/tv/88396-falcon-winter-soldier", "https://www.themoviedb.org/tv/60735-the-flash", 
-                          "https://www.themoviedb.org/tv/85271-wandavision", "https://www.themoviedb.org/movie/410718-before-the-flood", "https://www.themoviedb.org/movie/800394-the-last-cruise", 
-                          "https://www.themoviedb.org/movie/799555-operation-varsity-blues"];
+let descricaoDestaques = ["Em uma era em que os monstros andam na Terra, a luta da humanidade por seu futuro bota Godzilla e Kong em confronto. Enquanto Monarch vai em uma missão em terreno desconhecido descobrindo pistas sobre as origens dos Titãs, uma conspiração ameaça tirar as criaturas da Terra para sempre.", 
+                         "Armado com apenas uma palavra – Tenet – e lutando pela sobrevivência do mundo inteiro, o Protagonista viaja através de um mundo crepuscular de espionagem internacional em uma missão que irá desenrolar em algo para além do tempo real.", 
+                         "Determinado a garantir que o sacrifício final do Superman não foi em vão, Bruce Wayne alinha forças com Diana Prince com planos de recrutar uma equipe de metahumanos para proteger o mundo de uma ameaça de proporções catastróficas que se aproxima.", 
+                         "Após os eventos de \"Vingadores: Ultimato\", Sam Wilson/Falcão e Bucky Barnes/Soldado Invernal se unem em uma aventura que testa suas habilidades - e a paciência.", 
+                         "Wanda Maximoff e Visão, um casal de super-heróis com uma vida perfeita, que começa a suspeitar que nem tudo é o que parece.", 
+                         "Barry enfrenta diversos assassinos que invadem a premiação onde ele acompanha Iris, mas os efeitos colaterais de seus novos superpoderes representam uma ameaça.", 
+                         "Um problema antigo está se mostrando mais grave. Mudanças de temperatura, inundações, e outros fenômenos semelhantes têm se tornado mais comuns, e o que se foi feito para amenizar essa situação? É na busca por respostas que Leonardo DiCaprio abraça a missão de ver as mudanças climáticas em diversos países, e de uma forma nunca vista antes.", 
+                         "Documentário que acompanha o primeiro e maior surto de COVID-19 fora da China: o do cruzeiro de luxo Diamond Princess.", 
+                         "Este documentário investiga o responsável por um esquema de suborno que garantia a entrada de herdeiros de famílias ricas e famosas nas melhores faculdades dos Estados Unidos."];
+
+let destaquesHrefPlay = ["https://www.youtube.com/watch?v=kIET9yMABD8", "https://www.youtube.com/watch?v=uXm6T1rAe28", 
+                         "https://www.youtube.com/watch?v=jt3-3LtSj6c", "https://www.youtube.com/watch?v=ZHaokMTWgdY", 
+                         "https://www.youtube.com/watch?v=e43b-f_Vg9I&pp=qAMBugMGCgJwdBAB", "https://www.youtube.com/watch?v=p__jH6NkowU",
+                         "https://www.youtube.com/watch?v=zFfurx3RZqo", "https://www.youtube.com/watch?v=iG5ni4b9QI0",
+                         "https://www.youtube.com/watch?v=LFHj8e7mU_I"];
+
+let destaquesHrefInfo = ["https://www.themoviedb.org/movie/399566-godzilla-vs-kong", "https://www.themoviedb.org/movie/577922-tenet",
+                         "https://www.themoviedb.org/movie/791373-zack-snyder-s-justice-league", 
+                         "https://www.themoviedb.org/tv/88396-falcon-winter-soldier", "https://www.themoviedb.org/tv/60735-the-flash", 
+                         "https://www.themoviedb.org/tv/85271-wandavision", "https://www.themoviedb.org/movie/410718-before-the-flood", "https://www.themoviedb.org/movie/800394-the-last-cruise", 
+                         "https://www.themoviedb.org/movie/799555-operation-varsity-blues"];
 
 //? Carroséis:
 let carroselFilmes = document.getElementById("carroselFilmes"); 
@@ -63,24 +76,79 @@ let carroselSeries = document.getElementById("carroselSeries");
 let carroselDocs = document.getElementById("carroselDocs");
 let carroselDestaques = document.getElementById("carroselDestaques");
 
-//? Destaque:
-let destaque = document.getElementsByClassName("destaque")[0];
+//? Gerador dos destaques
+for(let i=0; i<linksDestaques.length; i++){
+    let divDestaque = document.createElement("div");
+    divDestaque.className = "destaque";
+    divDestaque.id = "destaque";
+    
+    let divContainer = document.createElement("div");
+    divContainer.className = "container";
 
-destaque.style.background = "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url(" + destaquesLinks[0] + ")";
-destaque.style.backgroundSize = "cover";
+    let titulo = document.createElement("h3");
+    let tituloTexto = document.createTextNode(tituloDestaques[i]);
+    titulo.className = "titulo";
+
+    let descricao = document.createElement("p");
+    let descricaoTexto = document.createTextNode(descricaoDestaques[i]);
+    descricao.className = "descricao";
+    
+    let divBotoes = document.createElement("div");
+    divBotoes.className = "botoes";
+    
+    let linkTrailer = document.createElement("a");
+    linkTrailer.href = destaquesHrefPlay[i];
+    linkTrailer.target = "_blank";
+
+    let botaoPlay = document.createElement("button");
+    botaoPlay.className = "botao";
+    
+    let playSymbol = document.createElement("em");
+    playSymbol.className = "fas fa-play";
+
+    let textPlay = document.createTextNode("Assista o trailer agora");
+
+    let linkInfo = document.createElement("a");
+    linkInfo.href = destaquesHrefInfo[i];
+    linkInfo.target = "_blank";
+
+    let botaoInfo = document.createElement("button");
+    botaoInfo.className = "botao";
+
+    let infoSymbol = document.createElement("em");
+    infoSymbol.className = "fas fa-info-circle";
+
+    let textInfo = document.createTextNode("Mais informações");
+
+    botaoPlay.appendChild(playSymbol);
+    botaoPlay.appendChild(textPlay);
+    botaoInfo.appendChild(infoSymbol);
+    botaoInfo.appendChild(textInfo);
+    linkTrailer.appendChild(botaoPlay);
+    linkInfo.appendChild(botaoInfo);
+    divBotoes.appendChild(linkTrailer);
+    divBotoes.appendChild(linkInfo);
+    titulo.appendChild(tituloTexto);
+    descricao.appendChild(descricaoTexto);
+    divContainer.appendChild(titulo);
+    divContainer.appendChild(descricao);
+    divContainer.appendChild(divBotoes);
+    divDestaque.appendChild(divContainer);
+    carroselDestaques.appendChild(divDestaque);
+    divDestaque.style.background = "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)100%), url(" + linksDestaques[i] + ")";
+    divDestaque.style.backgroundSize = "cover";
+    divDestaque.style.backgroundPosition = "center";
+}
 
 //? Gerador dos filmes:
 for(let i = 0; i<linksFilme.length; i++){
-    //? Div do item:
     let divItemFilme = document.createElement("div");
     divItemFilme.className = "item";
     
-    //? Link:
     let linkFilme = document.createElement("a");
     linkFilme.href = filmesHref[i];
     linkFilme.target = "_blank";
 
-    //? Imagens:
     let imgFilme = document.createElement("img");
     imgFilme.className = "box";
     imgFilme.src = linksFilme[i];
--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,11 +1,12 @@
-$('.owl-carousel').owlCarousel({
+$('.owl-filmes-series-docs').owlCarousel({
     loop: false,
     margin: 10,
     nav: false, //? Setas de navegação
     dots: false,
     responsive: {
-        0: { //? Se for menor que 600px só uma imagem aparecerá
-            items: 1
+        0: { //? Se for menor que 600px só uma imagem aparecerá e uma imagem aparecerá na tela
+            items: 1,
+            center: true
         },
         600: { //? Se for menor ou igual que 600px, só três imagens aparecerarão
             items: 3
@@ -16,14 +17,25 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
-$('.owl-carousel-destaques').owlCarousel({
-    items: 1,
-    loop: false,
-    margin: 10,
-    autoplay: true,
-    autoplayHoverPause: true,
-    center: true,
+$('.owl-destaque').owlCarousel({
+    loop: true,
+    margin: 15,
     nav: false,
     dots: false,
-    rewind: true
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 3500,
+    mouseDrag: false,
+    touchDrag: false,
+    responsive: {
+        0: { //? Se for menor que 600px só uma imagem aparecerá
+            items: 1
+        },
+        600: { //? Se for menor ou igual que 600px, só três imagens aparecerarão
+            items: 1
+        },
+        1000: { //? Se for maior ou igual que 1000px, só cinco imagens aparecer
+            items: 1
+        }
+    }
 })
